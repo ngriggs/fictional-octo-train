1101970c0f6eabee07dc48f44e745d2dc9377bfc
Noah Griggs <noahg@vt.edu>
2022-09-18 13:09:20 -0500

    Finished
    could update navbar

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -19,7 +19,7 @@ const Business = () => (
   <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden"/>we'll handle the money</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>with the right credit card, you can improve your financial life by building credit, earnign rewards and saving money. But with hundreds of credit cards on the market.</p>
-    <Button styles="mt-10 mb-10 rounded-[10px]"/>
+    <Button styles="mt-10 mb-10"/>
 
     <div className={`${layout.sectionImg} flex-col`}>
       {features.map((feature, index) => (
